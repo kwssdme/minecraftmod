925359cbaad2e96a3d92a1b376a73f45eeb93608
kwssdme <1782622113@qq.com>
2025-08-26 10:58:29 +0800

    item

--- src/main/java/NewItem.java
+++
@@ -1,6 +0,0 @@
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
-
-public class NewItem {
-    public static final Item kwssdme = Items.register("kwssdme", new Item(new Item.Settings()));
-}
