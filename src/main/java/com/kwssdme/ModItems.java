f5c24b7e5354b642a390045f357a702ddba98e5d
kssdme <1782622113@qq.com>
2025-08-29 10:03:24 +0800

    fix cilent not work

--- src/main/java/com/kwssdme/ModItems.java
+++ src/main/java/com/kwssdme/ModItems.java
@@ -1,14 +1,13 @@
 package com.kwssdme;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-public final class ModItems<T> {
+public final class ModItems {
     private ModItems(){}
 
-    public static final Item KWSSD_ITEM = new Item(new FabricItemSettings());
+    public static final Item KWSSD_ITEM = register("kwss_item", new Item(new Item.Settings()));
 
     public static <T extends Item> T register(String path, T item){
         return Registry.register(Registries.ITEM, new Identifier("kwssdme", path), item);
