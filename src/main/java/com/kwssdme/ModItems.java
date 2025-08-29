3bd107b0289577df323792bec9abfaf5b0fa36d0
kssdme <1782622113@qq.com>
2025-08-29 17:36:03 +0800

    trytofixitemtex

--- src/main/java/com/kwssdme/ModItems.java
+++ src/main/java/com/kwssdme/ModItems.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 public final class ModItems {
     private ModItems(){}
 
-    public static final Item KWSSD_ITEM = register("kwss_item", new Item(new Item.Settings()));
+    public static final Item KWSSD_ITEM = register("kwssdme", new Item(new Item.Settings()));
 
     public static <T extends Item> T register(String path, T item){
         return Registry.register(Registries.ITEM, new Identifier("kwssdme", path), item);
