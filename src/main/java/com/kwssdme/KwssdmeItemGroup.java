348282dd454ebe470b13e790393e7d6185ac9d95
kssdme <1782622113@qq.com>
2025-09-03 17:11:25 +0800

    put item group and fix item tex

--- src/main/java/com/kwssdme/KwssdmeItemGroup.java
+++ src/main/java/com/kwssdme/KwssdmeItemGroup.java
@@ -11,11 +11,12 @@ import net.minecraft.util.Identifier;
 public final class KwssdmeItemGroup {
     public static final ItemGroup KWSSDME_ITEM_GROUP = FabricItemGroup.builder()
 
-           .icon(() -> new ItemStack(ModItems.shit))
-           .displayName(Text.translatable("itemGroup.kwssdme"))
-           .entries((context, entries)->{
-                entries.add(ModItems.shit);})
-           .build();
+            .icon(() -> new ItemStack(ModItems.shit))
+            .displayName(Text.translatable("itemGroup.kwssdme"))
+            .entries((context, entries)->{
+                entries.add(ModItems.shit);
+                entries.add(ModItems.vocild);})
+            .build();
 
     public static void initialize(){
         Registry.register(Registries.ITEM_GROUP, new Identifier("kwssdme", "kwssdme_item_group"), KWSSDME_ITEM_GROUP);
