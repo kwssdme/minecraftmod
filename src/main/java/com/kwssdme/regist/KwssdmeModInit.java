96b3226c7649b9bf0eab4126eee6c9f1e2dacd6d
kwssdme <1782622113@qq.com>
2025-09-06 17:26:15 +0800

    md daxiaoxie

--- src/main/java/com/kwssdme/regist/KwssdmeModInit.java
+++ src/main/java/com/kwssdme/regist/KwssdmeModInit.java
@@ -2,16 +2,16 @@ package com.kwssdme.regist;
 
 import com.kwssdme.moditemgroup.KwssdmeItemGroup;
 import com.kwssdme.tips.shit;
-import com.kwssdme.tips.vociload;
+import com.kwssdme.tips.vocaloid;
 
 import net.fabricmc.api.ModInitializer;
 
-public class KwssdmeModinit  implements ModInitializer{
+public class KwssdmeModInit  implements ModInitializer{
     @Override
     public void onInitialize(){
         ModItems.initialize();
         KwssdmeItemGroup.initialize();
         shit.initialize();
-        vociload.initialize();
+        vocaloid.initialize();
     }
 }
