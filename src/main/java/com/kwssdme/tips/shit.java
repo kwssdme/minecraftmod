be9cbd7eb190753d320c4d93fe5b26237e65eed7
kwssdme <1782622113@qq.com>
2025-09-06 17:18:31 +0800

    fuck Ss Vv

--- src/main/java/com/kwssdme/tips/shit.java
+++ src/main/java/com/kwssdme/tips/shit.java
@@ -1,21 +1,22 @@
 package com.kwssdme.tips;
 
 import java.util.List;
-
 import com.kwssdme.regist.ModItems;
-
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
-public final class Vocaloid {
+public final class shit {
 
-    public static final Item vocaloid = ModItems.registerItem("vocaloid", new Item(new Item.Settings()));
+    public static final Item shit  = ModItems.registerItem("shit", new Item(new Item.Settings()));
     
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(Text.translatable("tips.kwssdme.vocaloid"));
+        tooltip.add(Text.translatable("tips.kwssdme.shit"));
     }
+
     public static void initialize() {}
 }
+
+
--- src/main/java/com/kwssdme/tips/vocaloid.java
+++ src/main/java/com/kwssdme/tips/vocaloid.java
@@ -1,22 +1,21 @@
 package com.kwssdme.tips;
 
 import java.util.List;
+
 import com.kwssdme.regist.ModItems;
+
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
-public final class Shit {
+public final class vocaloid {
 
-    public static final Item shit  = ModItems.registerItem("shit", new Item(new Item.Settings()));
+    public static final Item vocaloid = ModItems.registerItem("vocaloid", new Item(new Item.Settings()));
     
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(Text.translatable("tips.kwssdme.shit"));
+        tooltip.add(Text.translatable("tips.kwssdme.vocaloid"));
     }
-
     public static void initialize() {}
 }
-
-
