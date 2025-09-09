4b2dbd3d1aea9b5dddef803a2793337cfaef48cf
kwssdme <1782622113@qq.com>
2025-09-09 16:59:54 +0000

    mzk

--- src/main/java/com/kwssdme/items/vocaloid.java
+++ src/main/java/com/kwssdme/items/vocaloid.java
@@ -1,15 +1,32 @@
 package com.kwssdme.items;
 
 import java.util.List;
+
+import com.kwssdme.regist.KwssdmeModInit;
 import com.kwssdme.regist.ModItems;
+
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public final class vocaloid extends Item{
+    public static final SoundEvent VOCALOID_SOUND = register("vocaloid_sound");
     public vocaloid(Settings settings) {super(settings);}
+
+
+    private static SoundEvent register(String name) {
+        Identifier id = new Identifier(KwssdmeModInit.MOD_ID, name);
+        return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
+    }
     
     public static final Item vocaloid = ModItems.registerItem("vocaloid", new vocaloid(new Item.Settings()));
     
@@ -17,5 +34,12 @@ public final class vocaloid extends Item{
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.translatable("tips.kwssdme.vocaloid"));
     }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(VOCALOID_SOUND, 1.0F, 1.0F);
+            return TypedActionResult.success(user.getStackInHand(hand));
+        }
+
     public static void initialize() {}
 }
--- src/main/java/com/kwssdme/regist/KwssdmeModInit.java
+++ src/main/java/com/kwssdme/regist/KwssdmeModInit.java
@@ -1,17 +1,27 @@
 package com.kwssdme.regist;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.kwssdme.items.shit;
 import com.kwssdme.items.vocaloid;
 import com.kwssdme.moditemgroup.KwssdmeItemGroup;
-
 import net.fabricmc.api.ModInitializer;
 
-public class KwssdmeModInit  implements ModInitializer{
+public class KwssdmeModInit implements ModInitializer{
+    public static final String MOD_ID = "kwssdme";
+
+    public static final  Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    
     @Override
     public void onInitialize(){
         ModItems.initialize();
         KwssdmeItemGroup.initialize();
         shit.initialize();
         vocaloid.initialize();
+
+
+
+        LOGGER.info("Hello sb logger");
     }
 }
